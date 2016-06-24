75475dd70f5b90c4dbe985c0477ad361506f1c6a
vladimir-sumy <vladimir.sumy@gmail.com>
2016-06-24 14:08:54 +0300

    Level03\lesson04\Task04

---
+++ src/com/javarush/test/level03/lesson06/task02/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.test.level03.lesson06.task02;
+
+/* Таблица умножения
+Выведи на экран таблицу умножения 10 на 10 в следующем виде:
+1 2 3 …
+2 4 6 …
+3 6 9 …
+…
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //напишите тут ваш код
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.println(1*i+" "+2*i+" "+3*i+" "+4*i+" "+5*i+" "+6*i+" "+7*i+" "+8*i+" "+9*i+" "+10*i);
+
+        }
+
+    }
+}
