0d5cfce4c72847752a3def27a8ec2ad339fb97e0
vladimir-sumy <vladimir.sumy@gmail.com>
2016-07-12 21:26:01 +0300

    4/4/9

--- src/com/javarush/test/level05/lesson12/bonus01/Solution.java
+++ src/com/javarush/test/level05/lesson12/bonus01/Solution.java
@@ -11,8 +11,8 @@ public class Solution
    public static void main(String[] args) throws Exception
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int a = reader.readLine();
-        int b = reader.read();
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
 
         int sum = a + b;
         System.out.println("Sum = " + sum);
