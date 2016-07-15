cb198d595db32db799376723b36a87ecc7ba7e1a
vladimir-sumy <vladimir.sumy@gmail.com>
2016-07-15 16:28:39 +0300

    6-8

---
+++ src/com/javarush/test/level06/lesson08/task03/Util.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level06.lesson08.task03;
+
+/* Класс Util
+Реализовать статический метод double getDistance(x1, y1, x2, y2). Он должен вычислять расстояние между точками.
+Используй метод double Math.sqrt(double a), который вычисляет квадратный корень переданного параметра
+*/
+
+public class Util
+{
+    public static double getDistance(int x1, int y1, int x2, int y2)
+    {
+        //напишите тут ваш код
+        int x = Math.abs(x1 - x2);
+        int y = Math.abs(y1 - y2);
+        return Math.sqrt(x*x+y*y);
+
+    }
+}
