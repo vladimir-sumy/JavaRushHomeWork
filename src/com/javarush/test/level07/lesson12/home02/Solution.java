8d44c2bdf4b7203de0925c78e67573d560f01e8b
vladimir-sumy <vladimir.sumy@gmail.com>
2016-07-23 13:43:01 +0300

    Level03\lesson04\Task04

---
+++ src/com/javarush/test/level07/lesson12/home02/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.test.level07.lesson12.home02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* Переставить M первых строк в конец списка
+Ввести с клавиатуры 2 числа N  и M.
+Ввести N строк и заполнить ими список.
+Переставить M первых строк в конец списка.
+Вывести список на экран, каждое значение с новой строки.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //напишите тут ваш код
+    }
+}
